7139e69a0d89cbb2d2f97292b7d908ade08e1843
Artsiom <artkostm@gmail.com>
2018-12-25 20:35:23 +0300

    small update

--- src/main/scala/by/artsiom/bigdata201/tweet/TweetProducer.scala
+++ src/main/scala/by/artsiom/bigdata201/tweet/TweetProducer.scala
@@ -5,13 +5,13 @@ import akka.kafka.scaladsl.Producer
 import akka.stream.{ActorMaterializer, OverflowStrategy}
 import akka.stream.scaladsl.{Flow, Source}
 import by.artsiom.bigdata201.tweet.config.TweetsConfig
-import com.danielasfregola.twitter4s.TwitterStreamingClient
+import com.danielasfregola.twitter4s.{StreamingClients, TwitterStreamingClient}
 import com.danielasfregola.twitter4s.entities.Tweet
 import com.danielasfregola.twitter4s.entities.enums.Language
-import com.github.plokhotnyuk.jsoniter_scala.core.{writeToArray, JsonValueCodec}
+import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, writeToArray}
 
 trait TweetProducer {
-  protected val twitterClient: TwitterStreamingClient
+  protected val twitterClient: StreamingClients
 
   def publish(config: TweetsConfig)(
     implicit producerSettings: ProducerSettings[PKey, PVal],
@@ -19,7 +19,7 @@ trait TweetProducer {
     tweetValueCodec: JsonValueCodec[Tweet]
   ) = {
     val streamRef = Source
-      .actorRef[Tweet](bufferSize = 10, overflowStrategy = OverflowStrategy.dropHead)
+      .actorRef[Tweet](bufferSize = 10, overflowStrategy = OverflowStrategy.backpressure)
       .via(
         Flow[Tweet].map(
           t =>
@@ -43,7 +43,7 @@ trait TweetProducer {
 
 object TweetProducer {
 
-  def apply(client: TwitterStreamingClient): TweetProducer = new TweetProducer {
-    override protected val twitterClient: TwitterStreamingClient = client
+  def apply(client: StreamingClients): TweetProducer = new TweetProducer {
+    override protected val twitterClient: StreamingClients = client
   }
 }
