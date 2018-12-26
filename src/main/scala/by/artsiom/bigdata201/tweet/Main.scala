331ec943b72f53ff6524dd2ccabd340e89220f59
Artsiom Chuiko <artkostm@gmail.com>
2018-12-26 16:24:58 +0300

    fix entry point

--- src/main/scala/by/artsiom/bigdata201/tweet/Main.scala
+++ src/main/scala/by/artsiom/bigdata201/tweet/Main.scala
@@ -2,6 +2,7 @@ package by.artsiom.bigdata201.tweet
 
 import akka.actor.ActorSystem
 import akka.kafka.ProducerSettings
+import akka.kafka.scaladsl.Producer
 import akka.stream.ActorMaterializer
 import by.artsiom.bigdata201.tweet.config.AppConfig
 import com.danielasfregola.twitter4s.TwitterStreamingClient
@@ -11,26 +12,43 @@ import org.apache.kafka.common.serialization.ByteArraySerializer
 import pureconfig.generic.auto._
 
 object Main extends App {
-//  implicit val system              = ActorSystem("tweet_publisher")
-//  implicit val mat                 = ActorMaterializer()
-//  implicit val tweetJsonValueCodec = JsonCodecMaker.make[Tweet](CodecMakerConfig())
-//  implicit val producerSettings =
-//    ProducerSettings[PKey, PVal](system, new ByteArraySerializer, new ByteArraySerializer)
-//
-//  val config = pureconfig.loadConfig[AppConfig]
-//  val client = TwitterStreamingClient()
-//
-//
-//  config.map(TweetProducer(client).publish(_))
+  implicit val system              = ActorSystem("tweet_publisher")
+  implicit val mat                 = ActorMaterializer()
+  implicit val tweetJsonValueCodec = JsonCodecMaker.make[Tweet](CodecMakerConfig())
+  implicit val producerSettings =
+    ProducerSettings[PKey, PVal](system, new ByteArraySerializer, new ByteArraySerializer)
+
+  val config = pureconfig.loadConfig[AppConfig]
+  val client = TwitterStreamingClient()
+
+
+  config.map(appConfig => TweetProducer(client).publish(appConfig.tweets, Producer.plainSink(producerSettings)))
 
   val ip = "128.42.5.4"
 
   val mask = "255.255.248.0"
 
-  val ip32bits = java.lang.Long.parseLong(ip.split("\\.").map(s => ("00000000" + (s.toInt.toBinaryString)).substring(s.toInt.toBinaryString.length)).mkString, 2)
+  val ip32bits = java.lang.Long.parseLong(
+    ip.split("\\.")
+      .map(s => ("00000000" + (s.toInt.toBinaryString)).substring(s.toInt.toBinaryString.length))
+      .mkString,
+    2
+  )
 
-  val mask32bits = java.lang.Long.parseLong(mask.split("\\.").map(s => ("00000000" + (s.toInt.toBinaryString)).substring(s.toInt.toBinaryString.length)).mkString, 2)
+  val mask32bits = java.lang.Long.parseLong(
+    mask
+      .split("\\.")
+      .map(s => ("00000000" + (s.toInt.toBinaryString)).substring(s.toInt.toBinaryString.length))
+      .mkString,
+    2
+  )
 
-  println((ip32bits & mask32bits).toBinaryString.toCharArray.grouped(8).map(_.mkString).map(Integer.parseInt(_, 2)).mkString("."))
+  println(
+    (ip32bits & mask32bits).toBinaryString.toCharArray
+      .grouped(8)
+      .map(_.mkString)
+      .map(Integer.parseInt(_, 2))
+      .mkString(".")
+  )
 
 }
--- src/main/scala/by/artsiom/bigdata201/tweet/package.scala
+++ src/main/scala/by/artsiom/bigdata201/tweet/package.scala
@@ -1,9 +1,20 @@
 package by.artsiom.bigdata201
 
+import java.util.Date
+
+import com.github.plokhotnyuk.jsoniter_scala.core.{JsonReader, JsonValueCodec, JsonWriter}
 import org.apache.kafka.clients.producer.ProducerRecord
 
 package object tweet {
   type PKey     = Array[Byte]
   type PVal     = Array[Byte]
   type PMessage = ProducerRecord[PKey, PVal]
+
+  implicit object DateJsonValueCodec extends JsonValueCodec[Date] {
+    override def decodeValue(in: JsonReader, default: Date): Date = new Date(in.readLong())
+
+    override def encodeValue(x: Date, out: JsonWriter): Unit = out.writeVal(x.getTime)
+
+    override def nullValue: Date = null
+  }
 }
