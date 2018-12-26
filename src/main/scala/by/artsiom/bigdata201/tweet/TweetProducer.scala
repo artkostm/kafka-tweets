49dacd95356165368e3903acc7ce3cffbdd47f44
Artsiom Chuiko <artkostm@gmail.com>
2018-12-26 16:24:07 +0300

    update producer

--- src/main/scala/by/artsiom/bigdata201/tweet/TweetProducer.scala
+++ src/main/scala/by/artsiom/bigdata201/tweet/TweetProducer.scala
@@ -1,34 +1,39 @@
 package by.artsiom.bigdata201.tweet
 
-import akka.kafka.ProducerSettings
-import akka.kafka.scaladsl.Producer
-import akka.stream.{ActorMaterializer, OverflowStrategy}
+import akka.stream.{ActorMaterializer, Graph, OverflowStrategy, SinkShape}
 import akka.stream.scaladsl.{Flow, Source}
 import by.artsiom.bigdata201.tweet.config.TweetsConfig
-import com.danielasfregola.twitter4s.{StreamingClients, TwitterStreamingClient}
-import com.danielasfregola.twitter4s.entities.{ExtendedTweet, Tweet}
+import com.danielasfregola.twitter4s.StreamingClients
+import com.danielasfregola.twitter4s.entities.Tweet
 import com.danielasfregola.twitter4s.entities.enums.Language
 import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, writeToArray}
 
 trait TweetProducer {
   protected val twitterClient: StreamingClients
 
-  def publish(config: TweetsConfig)(
-    implicit producerSettings: ProducerSettings[PKey, PVal],
-    mat: ActorMaterializer,
+  def publish(config: TweetsConfig,
+              kafkaProducerSink: Graph[SinkShape[PMessage], _])(
+    implicit mat: ActorMaterializer,
     tweetValueCodec: JsonValueCodec[Tweet]
   ) = {
     val streamRef = Source
-      .actorRef[Tweet](bufferSize = 10, overflowStrategy = OverflowStrategy.backpressure)
+      .actorRef[Tweet](config.streamBufSuze, OverflowStrategy.dropHead)
       .via(
-        Flow[Tweet].map(
-          t =>
-            new PMessage(config.topic,
-                         t.entities.map(_.hashtags.mkString(",")).getOrElse("").getBytes,
-                         writeToArray(t))
-        )
+        Flow[Tweet].flatMapConcat { tweet =>
+          val hashtags = for {
+            entities <- tweet.entities
+          } yield entities.hashtags
+
+          Source(hashtags.getOrElse(List.empty).toSet)
+            .map(
+              h =>
+                new PMessage(config.topic,
+                             (s"${tweet.user.map(_.name).getOrElse()}:${h.text}").getBytes,
+                             writeToArray(tweet))
+            )
+        }
       )
-      .to(Producer.plainSink(producerSettings))
+      .to(kafkaProducerSink)
       .run()
 
     twitterClient.filterStatuses(
@@ -38,6 +43,8 @@ trait TweetProducer {
     ) {
       case t: Tweet => streamRef ! t
     }
+
+    streamRef
   }
 }
 
