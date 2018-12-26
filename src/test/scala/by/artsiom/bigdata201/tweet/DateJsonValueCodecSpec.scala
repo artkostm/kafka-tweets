e89dceda751133cc1c384fe031997383fc04ac85
Artsiom Chuiko <artkostm@gmail.com>
2018-12-26 16:28:14 +0300

    date json value codec specs

--- src/test/scala/by/artsiom/bigdata201/tweet/DateJsonValueCodecSpec.scala
+++ src/test/scala/by/artsiom/bigdata201/tweet/DateJsonValueCodecSpec.scala
@@ -1,3 +1,26 @@
-package by.artsiom.bigdata201.tweet class DateJsonValueCodecSpec {
+package by.artsiom.bigdata201.tweet
 
+import java.util.Date
+
+import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodecMaker}
+import com.github.plokhotnyuk.jsoniter_scala.core.{readFromArray, writeToArray}
+import org.scalatest.FlatSpec
+
+class DateJsonValueCodecSpec extends FlatSpec {
+  case class TestCls(date: Date)
+
+  implicit val testClsCodec = JsonCodecMaker.make[TestCls](CodecMakerConfig())
+
+  "Codec" should "convert date to valid json Numeric (timestamp)" in {
+    val date = new Date()
+    val actualJson = new String(writeToArray(TestCls(date)))
+    assert(s"""{"date":${date.getTime}}""" == actualJson)
+  }
+
+  "Codec" should "convert json to correct date" in {
+    val date = new Date()
+    val json = s"""{"date":${date.getTime}}"""
+    val actualTestCls: TestCls = readFromArray(json.getBytes)(testClsCodec)
+    assert(date.getTime == actualTestCls.date.getTime)
+  }
 }
