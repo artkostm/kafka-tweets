e5e4151c24533989583943e43aad46d80242c5d6
Artsiom Chuiko <artkostm@gmail.com>
2018-12-26 16:27:19 +0300

    add tweet data generator

--- src/test/scala/by/artsiom/bigdata201/tweet/TweetGenerator.scala
+++ src/test/scala/by/artsiom/bigdata201/tweet/TweetGenerator.scala
@@ -1,3 +1,8 @@
-package by.artsiom.bigdata201.tweet object TweetGenerator {
+package by.artsiom.bigdata201.tweet
 
+import com.danielasfregola.randomdatagenerator.RandomDataGenerator
+import com.danielasfregola.twitter4s.entities.{Entities, HashTag, Tweet}
+
+object TweetGenerator extends RandomDataGenerator {
+  def rendomTweet(hashTag: String): Tweet = random[Tweet].copy(entities = Some(Entities(hashtags = List(HashTag(hashTag, List())))))
 }
