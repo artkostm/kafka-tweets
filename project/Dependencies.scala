e924c3dc4d4104e01e7634011c35ebd3b6d0251a
Artsiom Chuiko <artkostm@gmail.com>
2018-12-26 16:22:46 +0300

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,6 +7,12 @@ object Dependencies {
     val jsoniter   = "0.37.6"
     val pureconfig = "0.10.1"
     val twitter4s  = "5.5"
+    val akka = "2.5.13"
+
+    val scalaTest  = "3.0.5"
+    val scalaCheck = "1.14.0"
+    val scalaMock  = "4.1.0"
+    val randomDataGenerator = "2.6"
   }
 
   lazy val main = Seq(
@@ -16,4 +22,20 @@ object Dependencies {
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core"   % versions.jsoniter % Compile,
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % versions.jsoniter % Provided
   )
+
+  lazy val testCommon = Seq(
+    "org.scalatest"  %% "scalatest"                   % versions.scalaTest,
+    "org.scalacheck" %% "scalacheck"                  % versions.scalaCheck,
+    "org.scalamock"  %% "scalamock"                   % versions.scalaMock,
+    "com.danielasfregola" %% "random-data-generator" % versions.randomDataGenerator
+  )
+  
+  lazy val unitTests = (testCommon ++ Seq(
+    "com.typesafe.akka" %% "akka-stream-testkit" % versions.akka
+  )).map(_ % Test)
+
+  lazy val itTests = (testCommon ++ Seq(
+    "com.typesafe.akka" %% "akka-stream-testkit" % versions.akka,
+    "com.typesafe.akka" %% "akka-stream-kafka-testkit" % "1.0-RC1"
+  )).map(_ % IntegrationTest)
 }
