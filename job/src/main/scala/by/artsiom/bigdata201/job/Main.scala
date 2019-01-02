c00d4ea28725d10053d98733da55b33506a2357e
Artsiom Chuiko <artkostm@gmail.com>
2019-01-02 16:27:23 +0300

    job implementation

--- job/src/main/scala/by/artsiom/bigdata201/job/Main.scala
+++ job/src/main/scala/by/artsiom/bigdata201/job/Main.scala
@@ -1,41 +1,65 @@
 package by.artsiom.bigdata201.job
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.streaming.OutputMode
-import org.apache.spark.sql.types.BinaryType
+
+import java.nio.charset.StandardCharsets
+
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions._
+import pureconfig.generic.auto._
+import pureconfig.loadConfig
+
 
 object Main extends App {
 
-  val spark = SparkSession
-    .builder()
-    .master("local[*]")
-    .getOrCreate()
+  loadConfig[AppConfig].fold(
+    failure => sys.error(s"Please check your configuration: ${failure.toList.mkString(",")}"),
+    runJob(_, SparkSession.builder().getOrCreate())
+  )
 
-  val kafkaStreamDF = spark.readStream
-    .format("kafka")
-    .option("kafka.bootstrap.servers", "192.168.99.100:9092")
-    .option("subscribe", "Tweets")
-    .option("startingOffsets", "earliest")
-    .load()
+  def runJob(appConfig: AppConfig, spark: SparkSession): Unit = {
+    val Some((kafka, output)) = AppConfig.unapply(appConfig)
 
-  import spark.implicits._
+    // writing kafka topic to hdfs
+    spark.read
+      .format("kafka")
+      .option("kafka.bootstrap.servers", kafka.bootstrapServers)
+      .option("subscribe", kafka.topic)
+      .option("startingOffsets", kafka.startingOffsets)
+      .option("endingOffsets", kafka.endingOffsets)
+      .load()
+      .write
+      .mode(SaveMode.Append)
+      .option("checkpointLocation", kafka.checkpointLocation)
+      .parquet(output.kafkaDataHdfsPath)
 
-  kafkaStreamDF
-    .coalesce(2)
-    .select($"value".cast(BinaryType).as("tweet"), $"key".cast(BinaryType).as("key"))
-    .map { row =>
-      val tweet = new String(row.getAs[Array[Byte]]("tweet"))
-      val key   = new String(row.getAs[Array[Byte]]("key"))
-      (key.split(":")(1), tweet)
-    }
-    .withColumnRenamed("_1", "tag")
-    .writeStream
-    .partitionBy("tag")
-    .format("parquet")
-    .queryName("parquet_files_to_hdfs")
-    .outputMode(OutputMode.Append())
-    .option("path", "testDir")
-    .option("checkpointLocation", "/tmp/st/")
-    .start()
+    val hdfsDF = spark.read
+      .parquet(output.kafkaDataHdfsPath)
 
-  spark.streams.awaitAnyTermination(100000)
+    import spark.implicits._
+
+    // converts byte array to utf-8 string
+    val toUtfString = udf((payload: Array[Byte]) => new String(payload, StandardCharsets.UTF_8))
+
+    // converts milliseconds to seconds
+    val toSeconds = udf((ms: Long) => (ms / 1000))
+
+    // writing hashtag counts to hdfs
+    hdfsDF
+      .select(
+        from_json(toUtfString($"value"), tweetSchema).as("tweet"),
+        split(toUtfString($"key"), ":").as("key")
+      )
+      .withColumn("tag", $"key"(1))
+      .withColumn("date", from_unixtime(toSeconds($"tweet.created_at"), "YYYY-MM-dd"))
+      .withColumn("hour", hour(from_unixtime(toSeconds($"tweet.created_at"), "yyyy-MM-dd HH:mm:ss")))
+      .groupBy(
+        "tag", "date", "hour"
+      )
+      .count()
+      .write
+      .mode(SaveMode.Overwrite)
+      .partitionBy("date", "hour")
+      .parquet(output.hashtagCountsPath)
+
+    spark.stop()
+  }
 }
