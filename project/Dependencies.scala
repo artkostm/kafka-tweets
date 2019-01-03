c27960ec471c63b79e6fcd8cd661edfccd8485e3
Artsiom Chuiko <artkostm@gmail.com>
2019-01-03 13:40:55 +0300

    update build scripts

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,13 @@ object Dependencies {
 
   lazy val excludeJpountz = ExclusionRule(organization = "net.jpountz.lz4", name = "lz4")
 
-  lazy val mainP = Seq(
+  lazy val spark = Seq(
+    "org.apache.spark" %% "spark-core" % versions.spark % Provided,
+    "org.apache.spark" %% "spark-sql"  % versions.spark % Provided,
+    "org.apache.spark"      %% "spark-sql-kafka-0-10" % versions.spark excludeAll (excludeJpountz),
+  )
+
+  lazy val mainPublisher = Seq(
     "com.danielasfregola"                   %% "twitter4s"             % versions.twitter4s,
     "com.github.pureconfig"                 %% "pureconfig"            % versions.pureconfig,
     "com.typesafe.akka"                     %% "akka-stream-kafka"     % versions.alpakka,
@@ -32,37 +38,49 @@ object Dependencies {
     "ch.qos.logback"                        % "logback-classic"        % versions.logback
   )
 
-  lazy val mainJ = Seq(
+  lazy val mainBatching = spark ++ Seq(
     "com.github.pureconfig" %% "pureconfig"           % versions.pureconfig,
-    "org.apache.spark"      %% "spark-sql-kafka-0-10" % versions.spark excludeAll (excludeJpountz),
     "org.apache.spark"      %% "spark-core"           % versions.spark % Provided,
     "org.apache.spark"      %% "spark-sql"            % versions.spark % Provided
   )
 
-  lazy val testCommon = Seq(
+  lazy val mainStreaming = spark ++ Seq(
+    "org.apache.spark" %% "spark-streaming" % versions.spark % Provided,
+    "com.github.pureconfig" %% "pureconfig"           % versions.pureconfig,
+    "org.apache.spark"      %% "spark-core"           % versions.spark % Provided,
+    "org.apache.spark"      %% "spark-sql"            % versions.spark % Provided, 
+    "org.apache.spark" %% "spark-streaming-kafka-0-10" % versions.spark
+  )
+
+  private lazy val testCommon = Seq(
     "org.scalatest"       %% "scalatest"             % versions.scalaTest,
     "org.scalacheck"      %% "scalacheck"            % versions.scalaCheck,
     "org.scalamock"       %% "scalamock"             % versions.scalaMock,
     "com.danielasfregola" %% "random-data-generator" % versions.randomDataGenerator
   )
 
-  lazy val unitTestsP = (testCommon ++ Seq(
+  lazy val unitTestsPublisher = (testCommon ++ Seq(
     "com.typesafe.akka" %% "akka-stream-testkit" % versions.akka
   )).map(_ % Test)
 
-  lazy val itTestsP = (testCommon ++ Seq(
+  lazy val itTestsPublisher = (testCommon ++ Seq(
     "com.typesafe.akka" %% "akka-stream-testkit"       % versions.akka,
     "com.typesafe.akka" %% "akka-stream-kafka-testkit" % versions.alpakka_testkit
   )).map(_ % IntegrationTest)
 
-  lazy val unitTestsJ = (testCommon ++ Seq(
+  lazy val unitTestsBatching = (testCommon ++ Seq(
     )).map(_ % Test)
 
-  lazy val itTestsJ = (testCommon ++ Seq(
+  lazy val itTestsBatching = (testCommon ++ Seq(
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core"      % versions.jsoniter,
     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros"    % versions.jsoniter,
     "com.danielasfregola"                   %% "twitter4s"                % versions.twitter4s,
     "net.manub"                             %% "scalatest-embedded-kafka" % versions.embeddedKafka,
     "com.typesafe.akka"                     %% "akka-stream-testkit"      % versions.akka
   )).map(_ % IntegrationTest)
+  
+  lazy val common = Seq(
+    "org.apache.spark" %% "spark-catalyst" % versions.spark % Provided,
+    "org.apache.spark"      %% "spark-sql"            % versions.spark % Provided intransitive()
+  )
 }
